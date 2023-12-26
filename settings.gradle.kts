bbf95c8d96a8cd949d66d08711a51a4137fc56a0
electrolytej <hawks.jamesf@gmail.com>
2023-12-26 12:42:03 +0800

    u

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,15 @@
-rootProject.name = "full-stack-project"
+rootProject.name = "spacecraft-kmp"
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     repositories {
+        mavenLocal()
+        maven { url = uri("https://s01.oss.sonatype.org/content/repositories/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/public/") } //central+jcenter
+        maven { url = uri("https://maven.aliyun.com/repository/google/") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+//        maven { url = uri("https://maven.oschina.net/content/groups/public/") }
+        maven { url = uri("https://jitpack.io") }
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         google()
         gradlePluginPortal()
@@ -12,6 +19,13 @@ pluginManagement {
 
 dependencyResolutionManagement {
     repositories {
+        mavenLocal()
+        maven { url = uri("https://s01.oss.sonatype.org/content/repositories/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/public/") } //central+jcenter
+        maven { url = uri("https://maven.aliyun.com/repository/google/") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+//        maven { url = uri("https://maven.oschina.net/content/groups/public/") }
+        maven { url = uri("https://jitpack.io") }
         google()
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
